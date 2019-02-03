9b4cee64f98afab582759c513eb339b8bbed3246
Noam Kleiner <nmkleiner@gmail.com>
2019-02-03 11:16:43 +0200

    small fixes

--- src/cmps/title-list.js
+++ src/cmps/title-list.js
@@ -1,37 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
 import ListItem from '@material-ui/core/ListItem';
-
 import * as actionCreator from '../store/actions/actions'
 
-class TitleList extends Component {
 
 
-    render() {
-        const { movies, isTitleListOpen, closeTitleList } = this.props
-        return (
-            <Drawer open={isTitleListOpen} onClose={closeTitleList} anchor="left">
-                <h4 className="capitalize text-center mt-20">jump to movie</h4>
-                <List className="title-list">
+const TitleList = ({ movies, isTitleListOpen, closeTitleList }) => {
 
-                    {
-                        movies.sort((movie1, movie2) => {
-                            if (movie1.title < movie2.title) return -1
-                            if (movie1.title > movie2.title) return 1
-                            return 0;
-                        }).map((movie, idx) =>
-                            <a onClick={closeTitleList} className="item" key={idx} href={'#' + movie.title}>
-                                <ListItem className="capitalize">{movie.title}</ListItem>
-                            </a>
-                        )
-                    }
-
-                </List>
-            </Drawer>
-        )
-    }
+    return (
+        <Drawer className="title-list" open={isTitleListOpen} onClose={closeTitleList} anchor="left">
+            <div className="wrapper">
+                <h4 className="capitalize">jump to movie</h4>
+                <Button onClick={closeTitleList}><i className="far fa-times-circle"></i></Button>
+            </div>
+            <List>
+                {
+                    movies.sort((movie1, movie2) => {
+                        if (movie1.title < movie2.title) return -1
+                        if (movie1.title > movie2.title) return 1
+                        return 0;
+                    }).map((movie, idx) =>
+                        <a onClick={closeTitleList} className="item" key={idx} href={'#' + movie.title}>
+                            <ListItem className="capitalize">{movie.title}</ListItem>
+                        </a>
+                    )
+                }
+            </List>
+        </Drawer>
+    )
 }
 function mapStateToProps(state) {
     return {
